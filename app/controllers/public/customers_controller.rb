0bff005fccae38b68408315b88a760bc9e0dcdda
EC2 Default User <ec2-user@ip-172-31-25-67.ap-northeast-1.compute.internal>
2022-11-05 10:43:26 +0000

    public/customerpage1st

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,22 @@
 class Public::CustomersController < ApplicationController
+  before_action :correct_customer, only: [:edit, :update]
   def show
+  end
+  
+  def delete
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id),notice: '会員情報を更新しました.'
+    else
+      render :edit
+    end
+
   end
 end
