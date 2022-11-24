5e6f631850b6f63eba2ad534353db965d0e4fb01
EC2 Default User <ec2-user@ip-172-31-25-67.ap-northeast-1.compute.internal>
2022-11-24 12:51:11 +0000

    public/cart_items/6th

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   has_one_attached :item_image
   belongs_to :genre
   has_many :cart_items
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
 
   def get_item_image(width,height)
     unless item_image.attached?
