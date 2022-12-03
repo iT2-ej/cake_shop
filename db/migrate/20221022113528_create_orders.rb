a4671868891c92614e091eb4855fe0ddd7c9ec08
EC2 Default User <ec2-user@ip-172-31-25-67.ap-northeast-1.compute.internal>
2022-12-03 13:00:12 +0000

    EC/orders/confirm/complete

--- db/migrate/20221022113528_create_orders.rb
+++ db/migrate/20221022113528_create_orders.rb
@@ -5,7 +5,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.string :shipping_postal, null: false
       t.text :shipping_address, null: false
       t.string :shipping_name, null: false
-      t.integer :postage, null: false
+      t.integer :postage, default: 800, null: false
       t.integer :payment_method, null: false
       t.integer :billing_amount, null: false
       t.integer :order_status, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2022_11_08_105937) do
     t.string "shipping_postal", null: false
     t.text "shipping_address", null: false
     t.string "shipping_name", null: false
-    t.integer "postage", null: false
+    t.integer "postage", default: 800, null: false
     t.integer "payment_method", null: false
     t.integer "billing_amount", null: false
     t.integer "order_status", null: false
