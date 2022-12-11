8fd4888289f4cf3db0de6244f36624c5ad7c4430
EC2 Default User <ec2-user@ip-172-31-25-67.ap-northeast-1.compute.internal>
2022-12-11 12:51:13 +0000

    next/admin_rayout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  
+
   has_many :order_details
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum order_status: { wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, finish: 4}
