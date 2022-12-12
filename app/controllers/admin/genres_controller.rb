99e3980b4a9b08c786bad2c783ac5e6007241ffb
EC2 Default User <ec2-user@ip-172-31-25-67.ap-northeast-1.compute.internal>
2022-12-12 12:45:11 +0000

    admin_layout_admin/customer/show

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -18,8 +18,8 @@ class Admin::GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
-    if @genre.update(post_params)
-      redirect_to request.referer
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
     else
       render :new
     end
